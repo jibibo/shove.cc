0752a1c77283e237d69cbe59a12791a5bc0e4c5e
Badr <badrkmc@gmail.com>
2021-03-06 18:49:09 +0500

    added smooth scrolling to bottom on new message

--- frontend/src/components/MessageBox.js
+++ frontend/src/components/MessageBox.js
@@ -1,31 +1,36 @@
-import { useState, useContext, useEffect } from "react";
+import { useState, useContext, useEffect, useRef } from "react";
 import { sendPacket, socket } from "../connection";
 import { GlobalContext } from "./GlobalContext";
 
 import "./MessageBox.css";
 
 function MessageBox() {
-    
+
     const { messages, setMessages, user } = useContext(GlobalContext);
 
     const [ message, setMessage ] = useState(""); 
 
-    console.log("MessageBox()");
+    const messageBox = useRef(null);
 
     function addMessage(text) {
         setMessages((messages) => [...messages, text]);
+
+        // TODO: why does this work??
+
+        messageBox.current.scrollTo({
+            top: messageBox.current.scrollHeight,
+            behavior: "smooth",
+        });
     }
 
     function sendMessage(event) {
         event.preventDefault();
-        console.log(user)
         sendPacket("chat_message", {
             username: user,
             content: message
         });
 
         setMessage("");
-
     }
 
     useEffect(() => {
@@ -33,8 +38,9 @@ function MessageBox() {
         socket.on("chat_message", (packet) => {
             console.debug("> MessageBox chat_message", packet);
             addMessage(
-                packet["username"] + ": " + packet["content"]
+                packet["username"] + " > " + packet["content"]
             );
+
         });
 
         socket.on("client_connected", (packet) => {
@@ -71,23 +77,25 @@ function MessageBox() {
     }, [socket]);
 
     return (
-        <>
+        <div className="messages-container">
             Messages:
-            <div className="message-box">
-            {messages.map((message, i) => 
-                (
-                    <div className="message" key={i}>
-                        <p>{message}</p>
-                    </div>
-                )
-            )}
-            </div>
-            
-            <form onSubmit={sendMessage}>
+
+                <div ref={messageBox} className="message-box">
+                    {messages.map((message, i) => 
+                        (
+                            <div className="message" key={i}>
+                                <img src="/img/avatar.png" alt="donald_duck" />
+                                <p>{message}</p>
+                            </div>
+                        )
+                    )}
+                </div>
+
+            <form className="message-input" onSubmit={sendMessage}>
                 <input type="textarea" onChange={(event) => setMessage(event.target.value)} value={message} />
             </form>
             
-        </>
+        </div>
     );
 }
 
