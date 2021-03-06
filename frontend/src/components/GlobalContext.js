e6125e03c8e63a343b6089e0dc2c9eeec5dd5021
Badr <badrkmc@gmail.com>
2021-03-06 17:39:13 +0500

    user is an empty string instead of undefined

--- frontend/src/components/GlobalContext.js
+++ frontend/src/components/GlobalContext.js
@@ -3,7 +3,7 @@ import { useState, createContext } from "react";
 const GlobalContext = createContext();
 
 function GlobalContextProvider({ children }) {
-    const [user, setUser] = useState();
+    const [user, setUser] = useState("");
     const [room, setRoom] = useState();
     const [messages, setMessages] = useState([]);
 
