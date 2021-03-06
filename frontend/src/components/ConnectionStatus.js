6687fb784b0c201ab2c7116fd9fa0474710a3089
Badr <badrkmc@gmail.com>
2021-03-06 17:39:23 +0500

    connection status validation

--- frontend/src/components/ConnectionStatus.js
+++ frontend/src/components/ConnectionStatus.js
@@ -2,43 +2,54 @@ import { useState } from "react";
 
 import { socket } from "../connection";
 
+import "./ConnectionStatus.css";
+
 let deaf = true;
 
 function ConnectionStatus() {
-    const [status, setStatus] = useState("Ready to connect");
-    const [sid, setSid] = useState();
+    const [status, setStatus] = useState({
+        text: "Ready to connect",
+        color: "yellow",
+    });
 
     if (deaf) {
         deaf = false;
 
         socket.on("connect", () => {
             console.debug("> ConnectionStatus connect event");
-            setStatus("Connected!");
-        });
-        socket.on("connect_error", () => {
-            console.debug("> ConnectionStatus connect_error event");
-            setStatus("Error on connect, backend offline?");
-        });
-        socket.on("disconnect", (reason) => {
-            console.debug("> ConnectionStatus disconnected event", reason);
-            setStatus("Disconnected! " + reason);
+            setStatus({
+                text: "Connected!",
+                color: "green",
+            });
         });
 
-        socket.on("client_connected", (packet) => {
-            console.debug("> ConnectionStatus client_connected", packet);
-            if (packet["you"]) {
-                setSid(packet["sid"]);
-            }
+        socket.on("connect_error", () => {
+            console.debug("> ConnectionStatus connect_error event");
+            setStatus({
+                text: "Error on connect, backend offline?",
+                color: "#f8d7da",
+            });
         });
+
+        socket.on("disconnect", (reason) => {
+            console.debug("> ConnectionStatus disconnected event", reason);
+            setStatus({
+                text: "disconnected" + reason,
+                color: "#f8d7da"
+            });
+        });
+
+        // socket.on("client_connected", (packet) => {
+        //     console.debug("> ConnectionStatus client_connected", packet);
+        //     if (packet["you"]) {
+        //         setSid(packet["sid"]);
+        //     }
+        // });
     }
 
     return (
-        <div className="website-status">
-            Connection status: {status}
-            <br />
-            Your sid: {sid}
-            <br />
-            <br />
+        <div style={{ backgroundColor: status.color }} className="status">
+            Connection status: {status.text}
         </div>
     );
 }
